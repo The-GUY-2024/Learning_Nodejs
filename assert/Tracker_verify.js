dcf3e01d977c5df92ba83c71b69ad61fe90d240b
Certified Humane <castrolwilliam@gmail.com>
2022-10-12 08:53:06 -0700

    Add files via upload

---
+++ assert/Tracker_verify.js
@@ -0,0 +1,18 @@
+const assert = require('node:assert');
+
+//Create calls tracker 
+
+const tracker = new assert.CallTracker();
+
+function func() {}
+
+
+
+// Returns a function that wraps func() that must be called exact times
+// before tracker.verify().
+const callsfunc = tracker.calls(func, 2);
+
+callsfunc();
+
+// Will throw an error since callsfunc() was only called once.
+tracker.verify();
